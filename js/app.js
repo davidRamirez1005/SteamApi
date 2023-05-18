b55ba1abcf614bf8aca72264e8b8e9867dcd4b19
davidRamirez1005 <ramirezjd1005@gmail.com>
2023-05-18 17:44:56 -0500

    feat: :zap: imagenes
    agregue imagenes por medio de fetch

--- js/app.js
+++ js/app.js
@@ -2,7 +2,6 @@
 let buscar = document.getElementById("buscar")
 
 
-const url = 'https://steam2.p.rapidapi.com/search/Counter/page/1';
 const options = {
 	method: 'GET',
 	headers: {
@@ -16,7 +15,7 @@ buscar.addEventListener(("click"),(el)=>{
   el.preventDefault()
 let search = document.getElementById("search").value
 
-const url = `https://youtube138.p.rapidapi.com/search/?q=${search}`;
+const url = 'https://steam2.p.rapidapi.com/search/Among%20Us/page/1';
  
 // Realizar la solicitud GET utilizando Fetch
 fetch(url,options)
@@ -27,11 +26,10 @@ fetch(url,options)
         throw new Error('Error en la solicitud. Código de estado: ' + response.status);
     }
 })
-.then(function (data) {
-    // Procesar la respuesta
-    // Aquí puedes acceder a los datos devueltos por la API y realizar las acciones necesarias
-    console.log(data);
-    obtenerId(data);
+.then(function (datos) {
+
+    console.log(datos);
+    obtenerId(datos);
 })
 .catch(function (error) {
     // Ha ocurrido un error durante la solicitud
@@ -42,71 +40,71 @@ document.getElementById("search").value = ""
 })
 
 function obtenerId(datos) {
-
-  const ID = datos.contents[0].video.videoId;
+  a =datos[0]
+  const ID = a.appId;
   console.log(ID);
-  document.getElementById("videoYoutube").src = `https://www.youtube-nocookie.com/embed/${ID}`;
+   document.getElementById("imgJuego").src = a.imgUrl;
+   
+   const title = a.title;
+   document.getElementById("title").innerHTML = title;
+   
+   const link = a.url;
+   document.getElementById("link").href = link;
+   
+   const precio = a.price;
+   document.getElementById("precio").innerHTML = precio;
+   
+   const fecha = a.released;
+   document.getElementById("fecha").innerHTML = fecha;
 
-  const ID1 = datos.contents[2].video.videoId;
-  console.log(ID1);
-  document.getElementById("relacion1").src = `https://www.youtube-nocookie.com/embed/${ID1}`;
+   b =datos[1]
+   const ID1 = b.appId;
+   console.log(ID1);
+    document.getElementById("otrasImagenes").src = b.imgUrl;
 
-  const ID2 = datos.contents[5].video.videoId;
-  console.log(ID2);
-  document.getElementById("relacion2").src = `https://www.youtube-nocookie.com/embed/${ID2}`;
+    const tituloOtros = b.title;
+    document.getElementById("tituloOtros").innerHTML = tituloOtros;
 
-  const ID3 = datos.contents[6].video.videoId;
-  console.log(ID3);
-  document.getElementById("relacion3").src = `https://www.youtube-nocookie.com/embed/${ID3}`;
+    const textOtro = b.reviewSummary;
+    document.getElementById("textOtro").innerHTML = textOtro;
 
-
-
-
-  const user = datos.contents[0].video.author.avatar[0].url;
-  document.getElementById("user").src = user;
-
-  const nombreCanal = datos.contents[0].video.author.title;
-  document.getElementById("nombreCanal").innerHTML = nombreCanal;
-
-  const titulo = datos.contents[0].video.title;
-  document.getElementById("tituloVideo").innerText = titulo;
 // -------------
-  const url2 = `https://youtube138.p.rapidapi.com/video/details/?id=${ID}&hl=en&gl=US`;
+//   const url2 = `https://youtube138.p.rapidapi.com/video/details/?id=${ID}&hl=en&gl=US`;
 
-  fetch(url2, options)
-    .then((respon) => respon.json())
-    .then((datos) => mostrardescripcion(datos, ID))
-    .catch(function (error) {
-      console.log('Error en la solicitud:', error.message);
-    });
-}
+//   fetch(url2, options)
+//     .then((respon) => respon.json())
+//     .then((datos) => mostrardescripcion(datos, ID))
+//     .catch(function (error) {
+//       console.log('Error en la solicitud:', error.message);
+//     });
+// }
 
 
-function mostrardescripcion(datos, ID) {
+// function mostrardescripcion(datos, ID) {
 
-  console.log(datos);
-  const description = datos.description;
-  console.log(description);
-  document.getElementById("descripcion").innerHTML = description;
+//   console.log(datos);
+//   const description = datos.description;
+//   console.log(description);
+//   document.getElementById("descripcion").innerHTML = description;
 
-  const url3 = `https://youtube138.p.rapidapi.com/video/comments/?id=${ID}&hl=en&gl=US`;
+//   const url3 = `https://youtube138.p.rapidapi.com/video/comments/?id=${ID}&hl=en&gl=US`;
 
-  fetch(url3, options)
-    .then((respon) => respon.json())
-    .then((datos) => mostrarComentarios(datos, ID))
-    .catch(function (error) {
-      console.log('Error en la solicitud:', error.message);
-    });
-}
+//   fetch(url3, options)
+//     .then((respon) => respon.json())
+//     .then((datos) => mostrarComentarios(datos, ID))
+//     .catch(function (error) {
+//       console.log('Error en la solicitud:', error.message);
+//     });
+// }
 
-function mostrarComentarios(datos) {
-  console.log(datos);
-  let coment = ""
-  for (const valor of datos.comments) {
-    coment += `<p>${valor.content}</p><hr>` 
-  }
-  console.log(coment);
-  document.getElementById("comentarios").innerHTML = coment
+// function mostrarComentarios(datos) {
+//   console.log(datos);
+//   let coment = ""
+//   for (const valor of datos.comments) {
+//     coment += `<p>${valor.content}</p><hr>` 
+//   }
+//   console.log(coment);
+//   document.getElementById("comentarios").innerHTML = coment
 
 
 }
--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
-const input = document.getElementById('gameName');
+const input = document.getElementById('search');
 const imagen = document.getElementById('imagen');
+const imagenJuego = document.getElementById('imgJuego');
+
 
 input.addEventListener('focus', () => {
   imagen.classList.add('blur');
+  imagenJuego.classList.add('blur');
 });
 
 input.addEventListener('blur', () => {
   imagen.classList.remove('blur');
+  imagenJuego.classList.remove('blur');
 });
 
