1e06e4b399746dc4f2e7991c72b56b74ace2268b
davidRamirez1005 <ramirezjd1005@gmail.com>
2023-05-19 11:52:34 -0500

    ci: :construction: subir
    agregar archivo js de boton y main en typo module

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+import scrollTopButton from "./boton.js";
+
+
 const input = document.getElementById('search');
 const imagen = document.getElementById('imagen');
 const imagenJuego = document.getElementById('imgJuego');
@@ -13,25 +16,7 @@ input.addEventListener('blur', () => {
   imagenJuego.classList.remove('blur');
 });
 
-const d = document,
-    w = window;
+document.addEventListener("DOMContentLoaded",()=>{
+  scrollTopButton(".scroll-top-btn")
+})
 
-export default function scrollTopButton(btn) {
-    const SCROLLBTN = d.querySelector(btn);
-
-    w.addEventListener("scroll", () => {
-        let scrollTop = w.pageYOffset || d.documentElement.scrollTop;
-        if (scrollTop < 600) {
-            SCROLLBTN.classList.add("hidden");
-        } else {
-            SCROLLBTN.classList.remove("hidden");
-        }
-    });
-
-    SCROLLBTN.addEventListener("click", () => {
-        w.scrollTo({
-            top: 0,
-            behavior: "smooth"
-        });
-    });
-}
